c3b343338a1b51806a72fa665d8125361af01043
Vaniza Marchetto <vaniza@outlook.com>
2023-10-10 10:09:29 -0300

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,34 @@
+export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const mode = "jit";
+export const darkMode = false;
+export const theme = {
+  fontFamily: {
+    display: ["Open Sans", "sans-serif"],
+    body: ["Open Sans", "sans-serif"]
+  },
+  extend: {
+    screens: {
+      mf: "990px"
+    },
+    keyframes: {
+      "slide-in": {
+        "0%": {
+          "-webkit-transform": "translateX(120%)",
+          transform: "translateX(120%)"
+        },
+        "100%": {
+          "-webkit-transform": "translateX(0%)",
+          transform: "translateX(0%)"
+        }
+      }
+    },
+    animation: {
+      "slide-in": "slide-in 0.5s ease-out"
+    }
+  }
+};
+export const variants = {
+  extend: {}
+};
+// eslint-disable-next-line no-undef
+export const plugins = [require("@tailwindcss/forms")];
